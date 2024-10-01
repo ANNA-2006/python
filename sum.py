96b8b9f114d5e99ea8bd566ae24a3ee759e8fdf7
annamariyashibu <annamariyashibu2028@cs.sjcetpalai.ac.in>
2024-10-01 15:29:45 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,10 @@
+""""Author=Anna
+date :01/10/2024
+used for finding the sum
+
+"""
+
+number1=int(input("enter first number:"))
+number2=int(input("Enter second number:"))
+sum=number1+number2
+print(sum)
