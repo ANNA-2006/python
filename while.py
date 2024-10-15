e381260135997564a595cfac7addf3966a6bd8b4
annamariyashibu <annamariyashibu2028@cs.sjcetpalai.ac.in>
2024-10-15 16:08:21 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,18 @@
+"""
+author: Anna mariya
+created on : 15/10/2024
+
+"""
+limit= int(input("enter the limit"))
+odd_number=1
+
+while odd_number<=limit:
+    print(odd_number,"\t",end="")
+
+    odd_number+=2
+
+
+
+
+
+
