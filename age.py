dd55bf3f791927d7ef2b11551c2a63d0b03448ae
annamariyashibu <annamariyashibu2028@cs.sjcetpalai.ac.in>
2024-10-15 14:59:46 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,15 @@
+"""
+author: Anna mariya
+created on : 15/10/2024
+
+"""
+age=int(input("enter the age"))
+if age<10:
+    print("fare is 2")
+elif age>=10 and age<60:
+    print("fare is 10")
+else :
+    print("fare is 5")
+
+
+
