cc73df3ed789731fe1a00ffc9be3085eb25d7282
annamariyashibu <annamariyashibu2028@cs.sjcetpalai.ac.in>
2024-10-15 15:55:05 +0530

    Add files via upload

---
+++ odd no.py
@@ -0,0 +1,18 @@
+"""
+author: Anna mariya
+created on : 15/10/2024
+
+"""
+limit= int(input("enter the limit"))
+odd_number=1
+count=0
+while count<limit:
+    print(odd_number,"\t",end="")
+    count+=1
+    odd_number+=2
+
+
+
+
+
+
